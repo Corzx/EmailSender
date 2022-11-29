1f93cceb3c9c70e8a73e2e026faa875d3c805f52
xCorz <corey_239@hotmail.com>
2022-11-29 19:44:09 +1100

    final

--- EmailSender.py
+++ EmailSender.py
@@ -1,8 +1,26 @@
 import os
+import smtplib
+from email.message import EmailMessage
 
-a = 12
-b = 14
-c = 15
-print(c)
-print(b)
-print(a)
+
+def auto_email():
+    login = "Enter your login here"
+    user = input("Please enter your name: ")
+    email = input("Please enter your email: ")
+    message = EmailMessage()
+    message.set_content(
+        f"""Dear {user},
+        Welcome to Python!"""
+    )
+    message["Subject"] = "It worked ?"
+    message["From"] = login
+    message["To"] = email
+    s = smtplib.SMTP("smtp.office365.com", 587)
+    s.starttls()
+    s.login(login, "Enter your password here")
+    s.send_message(message)
+    s.quit()
+    print("Email sent!")
+
+
+auto_email()
